41cc1bbde1484a3795773a6abf2f77f9659448b7
Xiang Ze <xchuay@gmail.com>
2023-08-06 17:41:43 +0800

    new commit

--- gg_app.py
+++ gg_app.py
@@ -2,10 +2,13 @@
 import streamlit as st
 import pickle
 import datetime
+import tensorflow as tf
 
 # Load the pre-trained model
 with open('xiang_model.h5', 'rb') as file:
     classifier = pickle.load(file)
+
+# classifier = tf.keras.models.load_model('xiang_model.h5')
 
 def main():
     st.title('Sleep Efficiency Prediction App')
@@ -14,7 +17,7 @@ def main():
     # Create input fields for user data
     age = st.slider('Age', 18, 100)
     gender = st.selectbox('Gender', ['Male', 'Female'])
-    caffeine_consumption = st.number_input('Caffeine Consumption (mg)', 0.0, 1000.0, 100.0)
+    caffeine_consumption = st.number_input('Caffeine Consumption (mg)', 0.0, 200.0, 25.0)
     alcohol_consumption = st.slider('Alcohol Consumption (drinks)', 0.0, 10.0, 0.0)
     smoking_status = st.selectbox('Smoking Status', ['Yes', 'No'])
     exercise_frequency = st.slider('Exercise Frequency (days per week)', 0, 7, 3)
@@ -47,19 +50,22 @@ def main():
     smoking_status_numeric = 1 if smoking_status == 'Yes' else 0
 
     # Make the prediction
-    prediction = classifier.predict([[age, gender_numeric, caffeine_consumption, alcohol_consumption,
-                                      smoking_status_numeric, exercise_frequency, bedtime_hour, wakeup_time_hour, light_sleep, rem_sleep, deep_sleep, awakenings, sleep_duration]])[0]
 
 #     light_sleep_percentage = 3
 #     rem_sleep_percentage = 3
 #     deep_sleep_percentage = 3
 #     prediction += light_sleep_percentage + rem_sleep_percentage + deep_sleep_percentage
 
+    if st.button("Predict"):
+        prediction = classifier.predict([[age, gender_numeric, caffeine_consumption, alcohol_consumption,
+                                      smoking_status_numeric, exercise_frequency, bedtime_hour, wakeup_time_hour, light_sleep, rem_sleep, deep_sleep, awakenings, sleep_duration]])[0]
+        st.success('The song is a {}'.format(prediction))
+
     # Ensure the predicted sleep efficiency is between 3 and 100
 #     prediction = max(3, min(prediction, 100))
 
     # Display the predicted sleep efficiency
-    st.write(f'Predicted Sleep Efficiency: {prediction:.2f}')
+#     st.write(f'Predicted Sleep Efficiency: {prediction:.2f}')
 
 # Run the Streamlit app
 if __name__ == '__main__':
